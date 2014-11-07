11bc120bbd6e9d8574f0d6519ca6a18145abf652
TristanSebillet <tristan.sebillet@bts-malraux.net>
2014-11-07 11:11:33 +0000

    Ajout du fichier DAO.java

---
+++ src/dao/DAO.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.sql.Connection;
+
+public abstract class DAO<T> {
+
+	//*************   ATTRIBUTS    *************
+	/**
+     * L'URL de la bdd.
+     *
+     * @see url
+     */
+	//public Connection connectDAO ;
+}
