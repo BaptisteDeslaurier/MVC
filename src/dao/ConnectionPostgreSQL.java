e7124d56f9c56aa5cd823050138d2b99636957e3
TristanSebillet <tristan.sebillet@bts-malraux.net>
2014-11-07 10:50:30 +0000

    Ajout de la connection à la bdd

---
+++ src/dao/ConnectionPostgreSQL.java
@@ -0,0 +1,53 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionPostgreSQL {
+
+	//*************   ATTRIBUTS    *************
+	/**
+     * L'URL de la bdd.
+     *
+     * @see url
+     */
+	private static String url = "jdbc:postgresql://172.16.99.2:5432/bdeslaurier";
+
+	/**
+     * L'utilisateur de la bdd.
+     *
+     * @see user
+     */
+	private static String user = "b.deslaurier";
+
+	/**
+     * Le mot de passe de l'utilisateur de la bdd.
+     *
+     * @see passwd
+     */
+	private static String passwd = "passe";
+
+	/**
+     * Un objet Connection
+     *
+     * @see connect
+     */
+	private static Connection connect;
+
+
+	//*************   METHODES   *************
+	/**
+     * Méthode getInstance() permet la connection à la base de données.
+     */
+	public static Connection getInstance(){
+		if(connect == null){
+			try {
+	    		connect = DriverManager.getConnection(url, user, passwd);
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		return connect;
+		}
+}
